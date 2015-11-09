f96598aa5a63a60364f2ab5a6b1351eaa54a6639
RomanGal <rgal@weezlabs.com>
2015-11-09 21:20:04 +0300

    Fix Example

---
+++ Example/Pods/Target Support Files/RWAuth/RWAuth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RWAuth : NSObject
+@end
+@implementation PodsDummy_RWAuth
+@end
---
+++ Example/Pods/Target Support Files/RWAuth/RWAuth-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double RWAuthVersionNumber;
+FOUNDATION_EXPORT const unsigned char RWAuthVersionString[];
+
